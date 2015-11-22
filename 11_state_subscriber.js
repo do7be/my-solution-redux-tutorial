f3ff5a00e88b16c1c2f3d384e4f4c8bb88103d08
do7be <bomsap700@gmail.com>
2015-11-22 23:21:09 +0900

    add 11

---
+++ 11_state_subscriber.js
@@ -0,0 +1,33 @@
+import { createStore, combineReducers } from 'redux'
+
+var itemsReducer = function (state = [], action) {
+    console.log('itemsReducer was called with state', state, 'and action', action)
+
+    switch (action.type) {
+        case 'ADD_ITEM':
+            return [
+                ...state,
+                action.item
+            ]
+        default:
+            return state;
+    }
+}
+
+var reducer = combineReducers({ items: itemsReducer })
+var store_0 = createStore(reducer)
+
+store_0.subscribe(function() {
+    console.log('store_0 has been updated. Latest store state:', store_0.getState());
+    // Update your views here
+})
+
+var addItemActionCreator = function (item) {
+    return {
+        type: 'ADD_ITEM',
+        item: item
+    }
+}
+
+store_0.dispatch(addItemActionCreator({ id: 1234, description: 'anything' }))
+
